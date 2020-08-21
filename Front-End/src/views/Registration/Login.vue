034510a668652c0b791be037d5fc66c4f3e2cd32
Tiran98 <49815897+Tiran98@users.noreply.github.com>
2020-08-21 20:58:27 +0530

    Login changes commited

--- Front-End/src/views/Registration/Login.vue
+++ Front-End/src/views/Registration/Login.vue
@@ -1,30 +1,32 @@
 <template>
-    <div id="ptLogin">
-        <div class="container login-container">
-            <div class="row">
-                <div class="col-md-6 login-form">
-                    <h3>Login for Form 1</h3>
-                    <form>
-                        <div class="form-group">
-                            <input type="text" class="form-control" placeholder="Your Email *" value="" />
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" placeholder="Your Password *" value="" />
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Sign In" />
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btnSubmit" value="Sign Up" />
-                        </div>
-                        <div class="form-group">
-                            <a href="#" class="ForgetPwd">Forget Password?</a>
-                        </div>
-                    </form>
+    <body>
+        <div id="ptLogin">
+            <div class="container login-container">
+                <div class="row">
+                    <div class="col-md-6 login-form">
+                        <h3 class="signin_txtTitle">Patient Login</h3>
+                        <form>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your Email *" value="" />
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" placeholder="Your Password *" value="" />
+                            </div>
+                            <div class="form-group">
+                                <a href="#" class="ForgetPwd">Forget Password?</a>
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btnSubmit signinbtn" value="Sign In" />
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" class="btnSubmit signupbtn" value="Sign Up" />
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </div>        
-    </div>
+            </div>        
+        </div>
+    </body>
 </template>
 
 <script>
@@ -34,5 +36,5 @@ export default {
 </script>
 
 <style scoped>
-
+    @import "../../assets/CSS/Login.css"
 </style>
