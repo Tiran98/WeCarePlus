9ab56dde751192ab86f99ec3eb022acbd12e367d
Tiran98 <49815897+Tiran98@users.noreply.github.com>
2020-08-24 05:02:35 +0530

    Back-End Database connection Done & commited

---
+++ Back-End/WeCarePlus/app/Patient.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Patient extends Eloquent
+{
+    protected $connection = 'mongodb';
+    protected $collection = 'Patient';
+
+    protected $fillable = [
+        'Patient_Name', 'Email','Password','Gender','Age','Address'
+    ];
+}
--- Back-End/WeCarePlus/app/User.php
+++ Back-End/WeCarePlus/app/User.php
@@ -28,6 +28,17 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function getJWTIdentifier()
+    {
+       return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
     /**
      * The attributes that should be cast to native types.
      *
