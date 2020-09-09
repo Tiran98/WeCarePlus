c16d14d4a3d5134f05c488ea4827883decbe8801
Heshy <udariheshani02@gmail.com>
2020-09-09 15:45:25 +0530

    Add channelin page

--- Front-End/src/router/index.js
+++ Front-End/src/router/index.js
@@ -3,10 +3,23 @@ import VueRouter from 'vue-router';
 import Login from '../views/Registration/Login.vue'
 import registation from '../views/Registration/patientRegistration.vue'
 import home from '../views/Home/home.vue'
+<<<<<<< Updated upstream
+=======
+import PatientList from '../views/Patient/PatientList.vue'
+import Missing from '../views/Missing.vue'
+// import WelcomePage from '../views/WelcomePage.vue'
+import AddNewPatient from '../views/Patient/AddNewPatient.vue'
+import AddChanneling from '../views/Channeling/AddChanneling.vue'
+import ChannelingList from '../views/Channeling/ChannelingList.vue'
+import TimeSet from '../views/Channeling/TimeSet.vue'
+import UpdateChannel from '../views/Channeling/UpdateChannel.vue'
+import AppointmentReceipt from '../views/Channeling/AppointmentReceipt.vue'
+>>>>>>> Stashed changes
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
+<<<<<<< Updated upstream
     mode: 'history',
     routes: [{
             path: "/register",
@@ -23,4 +36,80 @@ export default new VueRouter({
             component: home
         }
     ]
+=======
+  mode: 'history',
+  routes: [
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: Dashboard
+    },
+    {
+      path: '/patientList',
+      name: 'Patient List',
+      component: PatientList
+    },
+    {
+      path: '/addNewPatient',
+      name: 'Add New Patient',
+      component: AddNewPatient
+    },
+    {
+      path: '/about',
+      name: 'About',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+      path: '*',
+      component: Missing
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: registation
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/home",
+      component: home
+    },
+    {
+      path: "/AddChanneling",
+      name: "Add Channeling",
+      component: AddChanneling
+    },
+
+    {
+      path: "/ChannelingList",
+      name: "Channeling List",
+      component: ChannelingList
+    },
+
+    {
+      path: "/TimeSet",
+      name: "Time Set",
+      component:TimeSet
+    },
+
+    {
+      path: "/UpdateChannel",
+      name: "Update Channel",
+      component:UpdateChannel
+    },
+
+    {
+      path: "/AppointmentReceipt",
+      name: "Appointment Receipt",
+      component: AppointmentReceipt
+    },
+
+  ]
+>>>>>>> Stashed changes
 });
